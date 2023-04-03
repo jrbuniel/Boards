6160a97e92e0a5a3c4ed5df9a1ed745e2d6a03af
jrbuniel <jrbuniel15@icloud.com>
2023-04-03 19:01:54 +0800

    Added initial user interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Boards';
+  todoList:string[] = [];
+  inProgressList:string[] = [];
+  doneList:string[] = [];
+
+  addTask(task: string, listType: string) {
+    console.log('task', task);
+    if (listType === 'todo') {
+      this.todoList.push(task);
+    } else if (listType === 'doing') {
+      this.inProgressList.push(task);
+    } else if (listType === 'done') {
+      this.doneList.push(task);
+    }
+  }
+
+  deleteTask(task: string, listType: string) {
+    if (listType === 'todo') {
+      this.todoList.splice(this.todoList.indexOf(task), 1);
+    } else if (listType === 'doing') {
+      this.inProgressList.splice(this.inProgressList.indexOf(task), 1);
+    } else if (listType === 'done') {
+      this.doneList.splice(this.doneList.indexOf(task), 1);
+    }
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
